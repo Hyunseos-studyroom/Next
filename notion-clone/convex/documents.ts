5d3c2b531c952bbbbd677633e72fb99d363d7f65
3boku <hyunseojung@clika.io>
2024-10-18 16:36:59 +0900

    document list archive functionality

--- notion-clone/convex/documents.ts
+++ notion-clone/convex/documents.ts
@@ -1,19 +1,87 @@
 import {mutation, query} from "./_generated/server";
 import {v} from "convex/values";
+import {NormalizeError} from "next/dist/shared/lib/utils";
+import {Id} from "./_generated/dataModel";
 
-export const get = query({
-    handler: async (ctx) => {
+export const archive = mutation({
+    args: {
+        id: v.id("document")
+    },
+    handler: async (ctx, args) => {
         const identity = await ctx.auth.getUserIdentity();
 
-        if(!identity) {
+        if (!identity) {
             throw new Error("Not authenticated");
         }
 
-        const documents = await ctx.db.query("document").collect();
+        const userId = identity.subject;
 
-        return documents;
+        const existingDocument = await ctx.db.get(args.id);
+        if (!existingDocument) {
+            throw new NormalizeError("Document not found");
+        }
+
+        if (existingDocument.userId !== userId) {
+            throw new Error("Unauthrized");
+        }
+
+        const recursiveArchive = async (documentId: Id<"document">)=> {
+            const children = await ctx.db
+                .query("document")
+                .withIndex("by_user_parent", (q) => (
+                    q
+                        .eq("userId", userId)
+                        .eq("parentDocument", documentId)
+                ))
+                .collect();
+
+            for (const child of children) {
+                await ctx.db.patch(child._id, {
+                    isArchied: true,
+                })
+
+                await recursiveArchive(child._id);
+            }
+        }
+        const document = await ctx.db.patch(args.id, {
+            isArchied: true,
+        });
+
+        recursiveArchive(args.id);
+
+         return document;
     }
 })
+
+// @ts-ignore
+export const getSidebar = query({
+    args: {
+        parentDocument: v.optional(v.id("document"))
+    },
+    handler: async (ctx, args) => {
+        const identity = await ctx.auth.getUserIdentity();
+
+        if (!identity) {
+            throw new Error("Not authenticated");
+        }
+
+        const userId = identity.subject;
+
+        const document = await ctx.db
+            .query("document")
+            .withIndex("by_user_parent", (q) =>
+            q
+                .eq("userId", userId)
+                .eq("parentDocument", args.parentDocument)
+            )
+            .filter((q) => q.eq(q.field("isArchied"), false)
+            )
+            .order("desc")
+            .collect();
+
+        return document
+    },
+});
 
 export const create = mutation({
     args: {
