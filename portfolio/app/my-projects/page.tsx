7a0273853c470d4474d9f60467f371587ad58c49
3boku <hyunseojung@clika.io>
2024-10-25 13:13:06 +0900

    project page

--- portfolio/app/my-projects/page.tsx
+++ portfolio/app/my-projects/page.tsx
@@ -1,8 +1,17 @@
 "use client"
 
+import {Projects} from "@/constants";
+import ProjectCard from "@/components/ProjectCard";
+
 const Page = () => {
     return (
-        <div>page</div>
+        <div style={{backgroundImage: "url(/Mountains.jpg)"}} className={"w-screen h-screen flex items-center justify-center bg-center bg-cover"}>
+            <div className={"grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%] md:grid md:grid-cols-1/"}>
+                {Projects.map((project, index) => (
+                    <ProjectCard key={index} image={project.src} title={project.title} text={project.text} />
+                ))}
+            </div>
+        </div>
     )
 }
 
